1ecd5ecbcfae897803c8b5198141868ac6b4968d
torben82 <torben.green@gmail.com>
2014-05-23 14:04:30 +0200

    Update init.el
    Added python sjov

--- init.el
+++ init.el
@@ -330,9 +330,21 @@
 ;;  )
 ;; )
 
+;; Python fun:
 
-
-
+(setq visible-bell t)
+; Make sure your Emacs is 24 or newer.
+(require 'package)
+(require 'json)
+(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
+; Initialize package mode along with all the installed packages
+(package-initialize)
+; Enable elpy mode
+(elpy-enable)
+; Fixing a key binding bug in elpy
+(define-key yas-minor-mode-map (kbd "C-c k") 'yas-expand)
+; Fixing another key binding bug in iedit mode
+(define-key global-map (kbd "C-c o") 'iedit-mode)
 
 ;; Setting global key bindings.
 (global-set-key [f3] 'toggle-menubar)                     ;; Turns menubar on and off.
